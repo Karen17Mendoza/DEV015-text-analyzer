ff0a506a2bdc96a1b85e14a95565e12a5e09389a
karen <melanie171095@gmail.com>
2024-05-21 00:15:54 -0500

    segundo cambio

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 document.addEventListener('DOMContentLoaded', () => {
-    const textInput = document.getElementById('text-input');
+    const textInput = document.querySelector('textarea');
     const wordCountElement = document.querySelector('li[data-testid="word-count"]');
 
     textInput.addEventListener('input', () => {
