bfe687caa10e61637cc08648cf546a472ce923f4
karen <melanie171095@gmail.com>
2024-05-24 23:08:52 -0500

    Implementacion de GetNumberCount en index.js y index.html

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,7 +1,7 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    return text.trim() ? text.trim().split(/\s+/).length : 0;
+    return text.trim() ? text.trim().split(/\s+/).filter(text => isNaN(text)).length : 0;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
@@ -13,9 +13,11 @@ const analyzer = {
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    return text.match(/\b\d+\b/g) || [].length;
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -6,19 +6,20 @@ document.addEventListener('DOMContentLoaded', () => {
   const wordCountElement = document.querySelector('li[data-testid="word-count"]');
   const characterCountElement = document.querySelector('li[data-testid="character-count"]');
   const charCountExcludingSpacesElement = document.querySelector('li[data-testid="char-count-excluding-spaces"]');
-
+  const numberCountElement = document.querySelector('li[data-testid="number-count"]');
 
   textInput.addEventListener('input', () => {
     const text = textInput.value;
     const wordCount = analyzer.getWordCount(text);
     const characterCount = analyzer.getCharacterCount(text);
     const charCountExcludingSpaces = analyzer.getCharacterCountExcludingSpaces(text);
-    
+    const numberCount = analyzer.getNumberCount(text);
+
         
     wordCountElement.textContent = `Conteo de palabras: ${wordCount}`;
     characterCountElement.textContent = `Conteo de caracteres: ${characterCount}`;
     charCountExcludingSpacesElement.textContent = `Recuento de caracteres (sin espacios y signos de puntuación): ${charCountExcludingSpaces}`;
-        
+    numberCountElement.textContent = `Conteo de numeros: ${numberCount}`;       
   });
 });
 
