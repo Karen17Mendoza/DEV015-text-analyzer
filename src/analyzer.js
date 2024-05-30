a5e4f73f165b9f5cf043062879b4bd4db5b91523
karen <melanie171095@gmail.com>
2024-05-29 21:25:36 -0500

    Actualizacion de selectores en CSS

--- src/analyzer.js
+++ src/analyzer.js
@@ -13,16 +13,29 @@ const analyzer = {
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-
-  },
+    let words = text.match(/\b\w+\b/g) || [];
+    let totalLength = words.reduce((acc, word) => acc + word.length, 0);
+    let averageLength = totalLength / words.length;
+    let roundedAverageLength = Math.round(averageLength * 100) / 100;
+    return roundedAverageLength;
+    },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-    return (text.match(/\b\d+\b/g) || []).length;
+    let count = 0;
+    const words = text.split(/\s+/);
+
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        if (!isNaN(word) && word.trim() !== '') {
+            count++;
+        }
+    }
+    return count;
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-    const numbers = text.match(/\b\d+\b/g) || [];
-    return numbers.reduce((sum,num) => sum + parseInt(num,10),0);
+    const numbers = text.match(/\b\d+(\.\d+)?\b/g) || [];
+    return numbers.reduce((asc,num) => asc + parseFloat(num,10),0);
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const textInput = document.querySelector('textarea');
   const wordCountElement = document.querySelector('li[data-testid="word-count"]');
   const characterCountElement = document.querySelector('li[data-testid="character-count"]');
-  const charCountExcludingSpacesElement = document.querySelector('li[data-testid="char-count-excluding-spaces"]');
+  const charCountExcludingSpacesElement = document.querySelector('li[data-testid="character-no-spaces-count"]');
   const numberCountElement = document.querySelector('li[data-testid="number-count"]');
   const numberSumElement = document.querySelector('li[data-testid="number-sum"]');
+  const roundedAverageLengthElement = document.querySelector('li[data-testid="word-length-average"]');
+  const resetButton = document.getElementById('reset-button');
 
   textInput.addEventListener('input', () => {
     const text = textInput.value;
@@ -16,14 +18,27 @@ document.addEventListener('DOMContentLoaded', () => {
     const charCountExcludingSpaces = analyzer.getCharacterCountExcludingSpaces(text);
     const numberCount = analyzer.getNumberCount(text);
     const numberSum = analyzer.getNumberSum(text);
-
-
-        
+    const roundedAverageLength = analyzer.getAverageWordLength(text);
+            
     wordCountElement.textContent = `Conteo de palabras: ${wordCount}`;
     characterCountElement.textContent = `Conteo de caracteres: ${characterCount}`;
     charCountExcludingSpacesElement.textContent = `Conteo de caracteres (sin espacios y signos de puntuación): ${charCountExcludingSpaces}`;
     numberCountElement.textContent = `Conteo de numeros: ${numberCount}`;     
     numberSumElement.textContent =   `Suma de numeros: ${numberSum}`;
+    roundedAverageLengthElement.textContent = `Longitud media: ${roundedAverageLength}`;
+        
   });
+  resetButton.addEventListener('click', () => {
+    textInput.value = '';
+    textInput.placeholder = 'Escribe algo aquí...';
+    wordCountElement.textContent = 'Conteo de palabras: 0';
+    characterCountElement.textContent = 'Conteo de caracteres: 0';
+    charCountExcludingSpacesElement.textContent = 'Conteo de caracteres (sin espacios y signos de puntuación): 0';
+    numberCountElement.textContent = 'Conteo de números: 0';
+    numberSumElement.textContent = 'Suma de números: 0';
+    roundedAverageLengthElement.textContent = 'Longitud media: 0';
+  });
+
+
 });
 
