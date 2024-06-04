f5c87bf3ffef05b7e401defb9353f8ef3fe2e12a
karen <melanie171095@gmail.com>
2024-06-03 23:39:22 -0500

    actualizacion de archivos

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,7 +1,14 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    return text.trim() ? text.trim().split(/\s+/).length : 0;
+    const words = text.split(/\s+/);
+    let count = 0;
+    for (let i = 0; i < words.length; i++) {
+        if (isNaN(words[i]) && words[i].trim() !== '') {
+            count++;
+        }
+    }
+    return count;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
@@ -13,12 +20,30 @@ const analyzer = {
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.split(/\s+/).filter(word => word !== '');
+    const totalLength = words.reduce((sum, word) => sum + word.length, 0);
+    const averageLength = totalLength / words.length;
+    const roundedAverageLength = Math.round(averageLength * 100) / 100;
+    return roundedAverageLength;
+
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    let count = 0;
+    const words = text.split(/\s+/);
+
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i].replace(/\.$/, ''); // Eliminar el punto final si existe
+      if (/^\d+(\.\d+)?$/.test(word.trim())) {
+        count++;
+      }
+    }
+    return count;
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const numbers = text.match(/\b\d+(\.\d+)?\b/g) || [];
+    return numbers.reduce((asc,num) => asc + parseFloat(num,10),0);
   },
 };
 
